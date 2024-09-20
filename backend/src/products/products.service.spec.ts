e6cc331ca6fc76d1c8f1652748ce55382f643d51
Maria Sol Margarit <solmargaritapple@gmail.com>
2024-09-20 13:56:24 -0300

    - Modifico el test y le agrego el controller

--- backend/src/products/products.service.spec.ts
+++ backend/src/products/products.service.spec.ts
@@ -1,16 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ProductsService } from './products.service';
 import { PrismaService } from '../prisma/prisma.service';
+import { ProductsController } from './products.controller';
 
 describe('ProductsService', () => {
   let service: ProductsService;
+  let controller: ProductsController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      controllers: [ProductsController],
       providers: [ProductsService, PrismaService],
     }).compile();
 
     service = module.get<ProductsService>(ProductsService);
+    controller = module.get<ProductsController>(ProductsController);
   });
 
   it('should be defined', () => {
