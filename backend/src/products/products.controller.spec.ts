d00ef0cc962cf954339216cfdc3ed7c5eed62b49
Maria Sol Margarit <solmargaritapple@gmail.com>
2024-09-20 10:19:44 -0300

    - Arreglo 2 test que estaban rotos

--- backend/src/products/products.controller.spec.ts
+++ backend/src/products/products.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ProductsController } from './products.controller';
 import { ProductsService } from './products.service';
+import { PrismaService } from '../prisma/prisma.service';
 
 describe('ProductsController', () => {
   let controller: ProductsController;
@@ -8,7 +9,7 @@ describe('ProductsController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ProductsController],
-      providers: [ProductsService],
+      providers: [ProductsService, PrismaService],
     }).compile();
 
     controller = module.get<ProductsController>(ProductsController);
--- backend/src/products/products.service.spec.ts
+++ backend/src/products/products.service.spec.ts
@@ -1,12 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ProductsService } from './products.service';
+import { PrismaService } from '../prisma/prisma.service';
 
 describe('ProductsService', () => {
   let service: ProductsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ProductsService],
+      providers: [ProductsService, PrismaService],
     }).compile();
 
     service = module.get<ProductsService>(ProductsService);
