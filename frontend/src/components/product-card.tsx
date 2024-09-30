37a691ab6d3d4867ca3a664c638c936046fc4ede
Maria Sol Margarit <solmargaritapple@gmail.com>
2024-09-30 13:41:22 -0300

    - Modifico el CSS de la visualizacio de la imagen - Modifico el archivo para que quede organizado

--- frontend/src/components/product-card.tsx
+++ frontend/src/components/product-card.tsx
@@ -7,50 +7,54 @@ import { useRouter } from "next/navigation";
 export function ProductCard({ product }: any) {
   const router = useRouter()
 
-  async function handleRemoveProduct(id: string){
+  async function handleRemoveProduct(id: string) {
     await deleteProduct(id)
     router.refresh()
   }
 
-    return(
-        <Card onClick={() => {
-          router.push(`/products/${product.id}`)
-        }}>
+  return (
+    <Card onClick={() => {
+      router.push(`/products/${product.id}`)
+    }}>
 
-        <CardHeader>
-            <CardTitle className="flex justify-between">
-                {product.name}
-                <span className="text-sm font-bold text-gray-500">
-                    ${product.price}
-                </span>
-            </CardTitle>
-        </CardHeader>
+      <CardHeader>
+        <CardTitle className="flex justify-between">
+          {product.name}
+          <span className="text-sm font-bold text-gray-500">
+            ${product.price}
+          </span>
+        </CardTitle>
+      </CardHeader>
 
-        <img src={product.image} alt="Aca va una foto referida al producto" />
-        
-        <CardContent>
-          <p>
-            {product.description}
-          </p>
-        </CardContent>
-        <CardFooter className="flex justify-between">
-          <Button className="mt-5"
-            onClick={(e) => {
-              e.stopPropagation()
-              router.push(`/products/${product.id}/edit`)
-            }}
-          >
-            Editar
-          </Button>
-          <Button className="mt-5" variant="destructive"
-            onClick={(e) =>{
-              e.stopPropagation()
-              handleRemoveProduct(product.id)
-            }}
-          >
-            Eliminar
-          </Button>
-        </CardFooter>
-      </Card>
-    )
+      <img
+        src={product.image}
+        alt="Aca va una foto referida al producto"
+        style={{ maxWidth: '100%', height: '270px', width: '332px' }}
+      />
+
+      <CardContent>
+        <p>
+          {product.description}
+        </p>
+      </CardContent>
+      <CardFooter className="flex justify-between">
+        <Button className="mt-5"
+          onClick={(e) => {
+            e.stopPropagation()
+            router.push(`/products/${product.id}/edit`)
+          }}
+        >
+          Editar
+        </Button>
+        <Button className="mt-5" variant="destructive"
+          onClick={(e) => {
+            e.stopPropagation()
+            handleRemoveProduct(product.id)
+          }}
+        >
+          Eliminar
+        </Button>
+      </CardFooter>
+    </Card>
+  )
 }
