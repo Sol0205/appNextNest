511f18bf2fa0b9121d41cfdf51ed80cb1195f2ad
Maria Sol Margarit <solmargaritapple@gmail.com>
2024-10-02 01:22:28 -0300

    - Cambio el idioma del titulo a ingles

--- frontend/src/app/admin/products/page.tsx
+++ frontend/src/app/admin/products/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link"
 function ProductsAdmin() {
     return (
         <div>
-            <h1>Aca va la lista de productos</h1>
+            <h1>Here is the list of products</h1>
             <br />
             <Link
                 className={buttonVariants()}
