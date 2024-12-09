2a94d394d70969120bec05aa5456316ad112cfa1
Maria Sol Margarit <solmargaritapple@gmail.com>
2024-12-09 10:14:25 -0300

    - Importo el componente ProductDetail y arreglo los errores

--- frontend/src/app/catalog/product/[id]/detail/page.tsx
+++ frontend/src/app/catalog/product/[id]/detail/page.tsx
@@ -1,60 +1,22 @@
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { getProduct } from "@/app/products/products.api";
-import Link from "next/link";
-import { buttonVariants } from "@/components/ui/button";
-import './page.css'
 import NavBar from "@/app/navBar/page";
-
+import ProductDetail from "@/components/product-detail";
+import { getProduct } from "@/app/products/products.api";
 
 interface Props {
-    params: {
-        id: string
-    }
+    params: { id: string };
 }
 
-async function ProductDetailPage({ params }: Props) {
-    const product = await getProduct(params.id)
+export default async function ProductDetailPage({ params }: Props) {
+    const product = await getProduct(params.id);
+
+    if (!product) {
+        return <p>Product not found</p>;
+    }
 
     return (
-        <div className="flex justify-center items-center h-screen">
+        <div className="flex flex-col justify-center items-center h-screen">
             <NavBar />
-            <Card
-                className="card-content"
-            >
-                <CardHeader>
-                    <CardTitle className="flex justify-between">
-                        Product detail: {product.id}
-                        <Link
-                            className={buttonVariants()}
-                            href="../../../../catalog"
-                        >
-                            Go back
-                        </Link>
-                    </CardTitle>
-                </CardHeader>
-                <CardContent>
-                    <p>
-                        <span style={{ textDecoration: 'underline' }}>Nombre:</span> {product.name}
-                        <br />
-                        <span style={{ textDecoration: 'underline' }}>Descripción:</span> {product.description}
-                        <br />
-                        <span style={{ textDecoration: 'underline' }}>Color:</span> {product.color}
-                        <br />
-                        <span style={{ textDecoration: 'underline' }}>Categoria:</span> {product.category}
-                        <br />
-                        <span style={{ textDecoration: 'underline' }}>Tamaño:</span> {product.dimensions}
-                        <br />
-                        <span style={{ textDecoration: 'underline' }}>Metodo de pago:</span> {product.currency}
-                        <br />
-                        <span style={{ textDecoration: 'underline' }}>Precio:</span> {product.price}
-                    </p>
-                    <img src={product.image} alt=""
-                        className="w-full h-64 object-cover image"
-                    />
-                </CardContent>
-            </Card>
+            <ProductDetail product={product} />
         </div>
-    )
+    );
 }
-
-export default ProductDetailPage
